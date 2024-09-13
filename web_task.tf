a7cd3af24f05bda95d8b85bb4e4014ad6412588f
Charleen Chiu <charleenchiu@msn.com>
2024-09-14 03:57:59 +0800

    執行ansible playbook時不用sudo，以免被問密碼

--- web_task.tf
+++ web_task.tf
@@ -213,7 +213,7 @@ resource "null_resource" "setupVol" {
   }
   */
   provisioner "local-exec" {
-    command = "(pwd && echo 'Starting Ansible playbook execution' && sudo ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${var.key_name}.pem -i '${aws_instance.myWebServer.public_ip},' master.yml -e 'file_sys_id=${aws_efs_file_system.myWebEFS.id}' && echo 'Ansible playbook execution completed')"
+    command = "(pwd && echo 'Starting Ansible playbook execution' && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${var.key_name}.pem -i '${aws_instance.myWebServer.public_ip},' master.yml -e 'file_sys_id=${aws_efs_file_system.myWebEFS.id}' && echo 'Ansible playbook execution completed')"
   }    
 }
 
