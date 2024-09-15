f6d3384c84c0d7c42967e380c540ed4a088ae7fb
Charleen Chiu <charleenchiu@msn.com>
2024-09-15 18:28:48 +0800

    為Ansible playbook加上註解及翻譯成shell指令，方便Debug。Terraform中查看私鑰檔被叫用來執行Ansible前的權限。

--- web_task.tf
+++ web_task.tf
@@ -195,6 +195,7 @@ resource "null_resource" "setupVol" {
     command = <<EOT
       (
         echo '${path.module}/${var.key_name}.pem' &&
+        echo 'ls -l ${path.module}/${var.key_name}.pem' &&
         echo 'Starting Ansible playbook execution' &&
         ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${path.module}/${var.key_name}.pem -i '${aws_instance.myWebServer.public_ip},' master.yml -e 'file_sys_id=${aws_efs_file_system.myWebEFS.id}' &&
         echo 'Ansible playbook execution completed!'
